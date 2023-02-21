eb36904ada432857e7276923f1249a98709ee35e
gillyboat <105951203+gillyboat@users.noreply.github.com>
2023-02-21 13:48:17 -0500

    Updated csv parsing and alphabetical sort
    Made csv parse into dictionaries and added sorting alphabetically

--- backend/flaskr/algorithms.py
+++ backend/flaskr/algorithms.py
@@ -6,19 +6,38 @@ def format_student_data(file_name):
     """converts a csv from synergy into the data used in the grouping algorithms
 
     Args:
-    file_name (str): The name of the file to be opened with file extension
+        file_name (str): The name of the file to be opened with file extension
 
     Returns:
-    data (array): List of eachs tudent with their corresponding information
+        students (array): Dictionaries of each student's information based on the csv file
     
     """
     file = open(file_name, 'rt')
     #last,first,...
 
-    data = loadtxt(file,dtype=np.str_, delimiter=',',skiprows=1)
+    data = loadtxt(file,dtype=np.str_, delimiter=',')
 
-    return data
+    students = []
+    for i in range(1,len(data)):
+        students.append({id:i})
+        
+        for j in range(len(data[i])):
+            students[data[0][j]] = data[i][j]
 
-def alphabetical_sort(students,reverse=False,first_name = False):
+    return students
+
+def alphabetical_student_sort(students,reverse=False,first_name = False):
+    """sorts students by either their first or last name
     
-    return students.sort(reverse=reverse,)
+    Args:
+        students (array): An array of dictionaries representing each student's data
+        reverse (bool): A boolean representing if the sorted array should be reversed or not
+        first_name (bool): A boolean representing if the first name of the students should be used for sorting instead of the last name
+
+    Return:
+        sorted (array): The students array sorted alphabetically with the given parameters
+    """
+    key = "Last"
+    if first_name:
+        key = "First"
+    return students.sort(reverse=reverse,key=key)
