cfb93f37a436e8d1080554edc3c849d9006e6f85
notewio <fzheng413@gmail.com>
2023-02-22 07:53:12 -0500

    basic database

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from . import db
+from flask import Flask, request
+from . import database
 import os
 
 def create_app():
@@ -17,11 +17,60 @@ def create_app():
   except OSError:
     pass
 
-  db.init_app(app)
-  
-  @app.route("/")
-  def index():
-      return "Hello World!"
+  database.init_app(app)
+
+  @app.route("/api/class/new", methods=["POST"])
+  def create_class():
+    db = database.get_db()
+    res = db.execute("""
+      INSERT INTO Classrooms (name)
+        VALUES ("{}")
+    """.format(request.args["name"]))
+    db.commit()
+    return res.fetchall()
+
+  @app.route("/api/class/list", methods=["GET"])
+  def list_classes():
+    db = database.get_db()
+    res = db.execute("""
+      SELECT * FROM Classrooms c
+    """)
+    return [dict(row) for row in res.fetchall()]
+
+  @app.route("/api/class/<class_id>/students", methods=["GET"])
+  def get_class_students(class_id):
+    db = database.get_db()
+    res = db.execute(f"""
+      SELECT s.* FROM Classrooms c
+        JOIN ClassroomStudentMap m ON m.classroom_id = c.id
+        JOIN Students s ON m.student_id = s.id
+        WHERE c.id = {class_id}
+    """)
+    return [dict(row) for row in res.fetchall()]
+
+  @app.route("/api/class/<class_id>/add_student", methods=["POST"])
+  def add_student(class_id):
+    db = database.get_db()
+
+    res1 = db.execute(f"""
+      INSERT INTO Students (first_name, last_name)
+        VALUES (
+          "{request.args["first_name"]}",
+          "{request.args["last_name"]}"
+        )
+        RETURNING id
+    """)
+    row = res1.fetchone()
+    (student_id, ) = row if row else None
+
+    res = db.execute(f"""
+      INSERT INTO ClassroomStudentMap
+        VALUES ({class_id}, {student_id})
+    """)
+
+    db.commit()
+    return res.fetchall()
+    
 
   return app
 
