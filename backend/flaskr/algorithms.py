c653201078215ce4d24ec74648de226f45804b6f
gillyboat <105951203+gillyboat@users.noreply.github.com>
2023-02-22 12:45:16 -0500

    Added grouping by size or amount
    Returns a list of groups containing students grouped with the corresponding parameters

--- backend/flaskr/algorithms.py
+++ backend/flaskr/algorithms.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 from numpy import loadtxt
 
 
@@ -26,7 +27,7 @@ def format_student_data(file_name):
 
     return students
 
-def student_sort(students,key="Last",reverse=False):
+def student_sort(students,key="Last name",reverse=False):
     """sorts students by either their first or last name
     
     Args:
@@ -38,3 +39,42 @@ def student_sort(students,key="Last",reverse=False):
         sorted (array): The students array sorted with the given parameters
     """
     return students.sort(reverse=reverse,key=key)
+
+def groups_of_fixed_size(students,n):
+    leftover = len(students)%n
+    total_groups = math.ceil(len(students)/n)
+    num_small_groups = n - leftover
+    groups = [] * n
+    group_num = 0
+    for i in range(len(students)):
+        if (total_groups - group_num > num_small_groups and len(groups[group_num]) < n) or (total_groups - group_num <= num_small_groups and len(groups[group_num]) < n-1):
+            groups[group_num].append(students[i])
+        else:
+            group_num += 1
+    return groups
+
+def groups_of_fixed_amount(students,k):
+    size = math.ceil(len(students)/k)
+    return groups_of_fixed_size(students,size)
+
+
+def group_students(students,group_amount=0,group_size=0,sort_by=False,reverse=False,weights=False):
+    """sorts students into groupd with the corresponding parameters
+
+    Args:
+        students (array): An array of dictionaries representing each student's data
+        group_amount (int): How many groups should the students be sorted inti
+        group_size (int): How many students should be in each group (will be overriden if group_amount is set)
+        sort_by (str): If students should be sorted, what key will be used
+        reverse (bool): A boolean representing if the sorted array should be reversed or not
+        weights (array): wieghts matrix for students (UNIMPLEMENTED)
+    """
+    if sort_by:
+        s = student_sort(students,key=sort_by,reverse=reverse)
+        if group_size > 0:
+            return groups_of_fixed_size(s,group_size)
+        elif group_size > 0:
+            return groups_of_fixed_amount(s,group_amount)
+
+    if group_size <= 0 and group_amount <= 0:
+        return False
