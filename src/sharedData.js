d02ce5ddf146ac29435dea5b420473376fef99e8
notewio <fzheng413@gmail.com>
2023-03-10 07:45:26 -0500

    Add functionality to Header
    converted it from function to class, so state can be used to change current page display

--- src/sharedData.js
+++ src/sharedData.js
@@ -8,13 +8,6 @@ export const user = {
   imageUrl:
     'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
 }
-export const navigation = [
-  { name: 'Weighted Groups', href: '/', current: true },
-  { name: 'Seating Chart', href: '/seating', current: false },
-  { name: 'Other1', href: '#', current: false },
-  { name: 'Other2', href: '#', current: false },
-  { name: 'Other3', href: '#', current: false },
-]
 export const userNavigation = [
   { name: 'Your Profile', href: '#' },
   { name: 'Settings', href: '#' },
