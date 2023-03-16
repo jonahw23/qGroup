17dcf1a6a0334d32f600156d817a01de1ff42715
gillyboat <105951203+gillyboat@users.noreply.github.com>
2023-03-16 12:46:42 -0400

    rotation finally works
    press a button to activate rotate mode and click on a seat to rotate it to the clicked angle

--- src/seating/SeatingEditor.js
+++ src/seating/SeatingEditor.js
@@ -6,6 +6,7 @@ export default class SeatingEditor extends React.Component {
   state = {
     furniture: [],
     rotate: 0,
+    id:0,
   }
 
   componentDidMount = () => {
@@ -35,10 +36,10 @@ export default class SeatingEditor extends React.Component {
     const rotate = this.state.rotate
     let furniture = [...this.state.furniture];
 
-    this.setState({ furniture: furniture });
+    this.setState({ furniture: furniture, id: index});
 
-    if (rotate) {
-      furniture[index].theta = (furniture[index].theta ? furniture[index].theta : 0) + 1
+    if (rotate){
+      //furniture[index].theta = (furniture[index].theta?furniture[index].theta:0) + 1
 
       throw new Error('No Drag');
 
@@ -65,11 +66,37 @@ export default class SeatingEditor extends React.Component {
 
     // post new coords... maybe debounce too?
 
-    this.setState({ furniture: furniture });
+    this.setState({ furniture: furniture, id: index });
   }
 
   onKeyPressed = (e) => {
     this.setState({ rotate: !this.state.rotate });
+  }
+  onMouseMove = (e) => {
+    const rotate = this.state.rotate
+    const index = this.state.id
+    const width = this.getWidth();
+
+    let furniture = [...this.state.furniture];
+    let f = furniture[index]
+    let boxWidth = 96
+
+    const target = e.target;
+
+    // Get the bounding rectangle of target
+    const rect = target.getBoundingClientRect();
+
+    // Mouse position
+    const x = e.clientX - (rect.left + rect.width/2);
+    const y = e.clientY - (rect.top+ rect.height/2);
+    
+    let theta = furniture[index].theta?furniture[index].theta:0
+
+    let angle = Math.atan2(y,x)
+
+    if(rotate && e.clientX > rect.left && e.clientX < rect.right && e.clientY < rect.bottom && e.clientY > rect.top){
+    furniture[index].theta = 90+angle * (180/Math.PI)//(furniture[index].theta?furniture[index].theta:0) + 10*(angle - dAngle) 
+    }
   }
 
 
@@ -91,7 +118,8 @@ export default class SeatingEditor extends React.Component {
           onDrag={(_event, element) => this.onDrag(f.id, element)}
         >
           <div className="clear-seat-element">
-            <div className="seat-element" style={{ rotate: (f.theta ? f.theta : 0) + "deg" }} onKeyDown={this.onKeyPressed} tabIndex={0}>
+            <div className="seat-element" style={{ rotate: (f.theta ? f.theta : 0) + "deg" }} onKeyDown={this.onKeyPressed} onMouseMove={this.onMouseMove}
+ tabIndex={0}>
               x: {f.x.toFixed(2)} y: {f.y.toFixed(2)}
             </div>
           </div>
