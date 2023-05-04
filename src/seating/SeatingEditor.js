36b5b71a3baaf4424cfa3e371f28b902bb551367
notewio <fzheng413@gmail.com>
2023-05-04 08:49:33 -0400

    full screen presentation mode for seating
    also refactored SeatingEditor to functional instead of class

--- src/seating/SeatingEditor.js
+++ src/seating/SeatingEditor.js
@@ -1,63 +1,62 @@
-import React from 'react';
-import { HandRaisedIcon, PlusIcon } from '@heroicons/react/24/outline'
+import { useEffect, useState, useRef } from "react";
+import { ArrowsPointingInIcon, ArrowsPointingOutIcon, HandRaisedIcon, PlusIcon } from '@heroicons/react/24/outline'
 import * as api from "../api.js";
 import Seat from "./Seat.js";
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
 
-export default class SeatingEditor extends React.Component {
-  state = {
-    furniture: [],
-    students: [],
-    furn_groups: [],
+export default function SeatingEditor({ groups }) {
 
-    width: 0,
+  let [furniture, setFurniture] = useState([]);
+  let [students, setStudents] = useState([]);
+  let [furn_groups, setFurn_groups] = useState([]);
+  let [width, setWidth] = useState(0);
+  let [selected, setSelected] = useState([]);
+  let [select_corner, setSelect_corner] = useState(undefined);
+  let [mode, setMode] = useState("select");
 
-    selected: [],
-    select_corner: undefined,
+  let divElement = useRef(null);
+  const fsHandle = useFullScreenHandle();
 
-    mode: "select",
-  }
 
-  componentDidMount = () => {
 
-    this.getData();
+  useEffect(() => {
 
-    this.setState({ width: this.divElement?.clientWidth ?? 0 });
+    getData();
+
+    setWidth(divElement.current?.clientWidth ?? 0);
     window.addEventListener("resize", () => {
-      this.setState({ width: this.divElement?.clientWidth ?? 0 });
+      console.log(divElement)
+      setWidth(divElement.current?.clientWidth ?? width);
     });
 
-  }
+  }, [])
 
-  getData = () => {
-
+  const getData = () => {
     api.call("GET", "/users/6/class/8/seating/1/furniture/get_furniture_loc")
-      .then(json => { this.setState({ furniture: json }) });
+      .then(json => { setFurniture(json) });
     api.call("GET", "/users/6/class/8/seating/1/furniture_groups")
-      .then(json => { this.setState({ furn_groups: json }) });
+      .then(json => { setFurn_groups(json) });
     api.call("GET", "/users/6/class/8/seating/1/students")
-      .then(json => { this.setState({ students: json }) });
-
+      .then(json => { setStudents(json) });
   }
 
-
-  // Buttons
-
-  mapStudents = () => {
+  const mapStudents = () => {
     api.call("POST", "/users/6/class/8/seating/1/students")
-      .then(() => { this.getData(); });
+      .then(() => { getData(); });
   }
-  clearStudents = () => {
+  const clearStudents = () => {
     api.call("DELETE", "/users/6/class/8/seating/1/students")
-      .then(() => { this.getData(); });
+      .then(() => { getData(); });
   }
-  mapFurniture = () => {
-    const num_groups = this.props.groups.length;
+  const mapFurniture = () => {
+    const num_groups = groups.length;
     api.call("POST", "/users/6/class/8/seating/1/group_furn",
       { num_groups: num_groups })
-      .then(() => { this.getData(); });
+      .then(() => { getData(); });
   }
-  addSeat = (e) => {
-    const bounds = this.divElement.getBoundingClientRect();
+
+  const addSeat = (e) => {
+    const bounds = divElement.current.getBoundingClientRect();
 
     if (e.clientX === undefined) { return }
     const x = (e.clientX - bounds.left) / bounds.width;
@@ -70,99 +69,92 @@ export default class SeatingEditor extends React.Component {
         x: x, y: y, theta: theta
       })
       .then(json => {
-        let furniture = [...this.state.furniture];
-        furniture.push({
+        let furn = [...furniture];
+        furn.push({
           furn_id: json.furn_id,
           x: x, y: y, theta: theta
         });
-        this.setState({ furniture: furniture });
+        setFurniture(furn);
       })
   }
 
-
-  // Mouse & keyboard
-
-  onMouseDown = (event) => {
-    if (this.state.mode === "place") {
-      this.addSeat(event);
+  const onMouseDown = (event) => {
+    if (mode === "place") {
+      addSeat(event);
     }
     else {
-      if (event.target === this.divElement) {
-        const rect = this.divElement.getBoundingClientRect();
+      if (event.target === divElement.current) {
+        const rect = divElement.current.getBoundingClientRect();
 
         const x = event.clientX ?? event.touches[0].clientX;
         const y = event.clientY ?? event.touches[0].clientY;
 
-        this.setState({
-          select_corner: {
-            x: (x - rect.left) / this.state.width,
-            y: (y - rect.top) / this.state.width
-          }
+        setSelect_corner({
+          x: (x - rect.left) / width,
+          y: (y - rect.top) / width
         });
       }
     }
   }
-  onMouseMove = (event) => {
-    if (this.state.select_corner) {
-      const rect = this.divElement.getBoundingClientRect();
+  const onMouseMove = (event) => {
+    if (select_corner) {
+      const rect = divElement.current.getBoundingClientRect();
 
-      const c = this.state.select_corner;
+      const c = select_corner;
       let x = event.clientX ?? event.touches[0].clientX;
       let y = event.clientY ?? event.touches[0].clientY;
-      x = (x - rect.left) / this.state.width;
-      y = (y - rect.top) / this.state.width;
+      x = (x - rect.left) / width;
+      y = (y - rect.top) / width;
 
       const x1 = Math.min(c.x, x); const x2 = Math.max(c.x, x);
       const y1 = Math.min(c.y, y); const y2 = Math.max(c.y, y);
 
-      this.setState({
-        selected: [
-          ...this.state.furniture
-            .filter(f =>
-              x1 < f.x && f.x < x2 &&
-              y1 < f.y && f.y < y2)
-            .map(x => x.furn_id)
-        ]
-      });
+      setSelected([
+        ...furniture
+          .filter(f =>
+            x1 < f.x && f.x < x2 &&
+            y1 < f.y && f.y < y2)
+          .map(x => x.furn_id)
+      ]);
     }
   }
-  onMouseUp = (event) => {
-    this.setState({ select_corner: undefined });
+  const onMouseUp = (event) => {
+    setSelect_corner(undefined);
   }
 
-  onStartDrag = (f) => {
-    if (this.state.selected.length <= 1) {
-      this.setState({ selected: [f.furn_id] });
+  const onStartDrag = (f) => {
+    if (selected.length <= 1 || !selected.includes(f.furn_id)) {
+      setSelected([f.furn_id]);
     }
   }
-  onDrag = (f, draggable) => {
-    let furniture = [...this.state.furniture];
-    let anchor = { ...furniture.find(x => x.furn_id === f.furn_id) };
+  const onDrag = (f, draggable) => {
+    let furn = [...furniture];
+    let anchor = { ...furn.find(x => x.furn_id === f.furn_id) };
 
-    this.state.selected.forEach(id => {
-      const index = furniture.findIndex(x => x.furn_id === id);
-      const offset_x = this.state.furniture[index].x - anchor.x;
-      const offset_y = this.state.furniture[index].y - anchor.y;
+    selected.forEach(id => {
+      const index = furn.findIndex(x => x.furn_id === id);
+      const offset_x = furn[index].x - anchor.x;
+      const offset_y = furn[index].y - anchor.y;
 
-      furniture[index].x = draggable.x / this.state.width + offset_x;
-      furniture[index].y = draggable.y / this.state.width + offset_y;
+      furn[index].x = draggable.x / width + offset_x;
+      furn[index].y = draggable.y / width + offset_y;
     });
 
-    this.setState({ furniture: furniture });
+    setFurniture(furn);
   }
-  onStopDrag = (f) => {
-    this.state.selected.forEach(id => {
-      const index = this.state.furniture.findIndex(x => x.furn_id === id);
+  const onStopDrag = (f) => {
+    selected.forEach(id => {
+      const index = furniture.findIndex(x => x.furn_id === id);
       api.call("PUT", `/users/6/class/8/seating/1/furniture/${id}/move_furn`,
         {
-          new_x: this.state.furniture[index].x,
-          new_y: this.state.furniture[index].y,
-          new_theta: this.state.furniture[index].theta,
+          new_x: furniture[index].x,
+          new_y: furniture[index].y,
+          new_theta: furniture[index].theta,
         });
     })
   }
 
-  onRotate = (f, event) => {
+  const onRotate = (f, event) => {
     // NOTE: this is real janky
     const rect = event.target.parentElement.parentElement.getBoundingClientRect();
 
@@ -172,87 +164,97 @@ export default class SeatingEditor extends React.Component {
     y -= rect.top + rect.height / 2;
     const angle = Math.atan2(y, x);
 
-    let furniture = [...this.state.furniture];
-    const index = furniture.findIndex(x => x.furn_id === f.furn_id);
-    furniture[index].theta = 90 + angle * (180 / Math.PI);
-    this.setState({ furniture: furniture });
+    let furn = [...furniture];
+    const index = furn.findIndex(x => x.furn_id === f.furn_id);
+    furn[index].theta = 90 + angle * (180 / Math.PI);
+    setFurniture(furn);
   }
 
 
-  // Render
 
-  render = () => {
-
-    const seats = this.state.furniture.map(f => {
-      const student = this.state.students.find(x => x.furn_id === f.furn_id);
-      const name = student ? `${student.first_name} ${student.last_name}` : "";
-
-      return (
-        <Seat
-          f={f}
-          width={this.state.width}
-          selected={this.state.selected.indexOf(f.furn_id) >= 0}
-          group={this.state.furn_groups.find(x => x.furn_id === f.furn_id)?.table_group_id}
-          student={name}
-          dragHandlers={{
-            onStart: (e, draggable) => this.onStartDrag(f),
-            onDrag: (e, draggable) => this.onDrag(f, draggable),
-            onStop: (e, draggable) => this.onStopDrag(f),
-          }}
-          rotateHandlers={{
-            onDrag: (e, draggable) => this.onRotate(f, e),
-            onStop: (e, draggable) => this.onStopDrag(f),
-          }} />
-      );
-    });
+  const seats = furniture.map(f => {
+    const student = students.find(x => x.furn_id === f.furn_id);
+    const name = student ? `${student.first_name} ${student.last_name}` : "";
 
     return (
-      <div className="h-full flex flex-col" >
+      <Seat
+        f={f}
+        width={width}
+        selected={selected.indexOf(f.furn_id) >= 0}
+        group={furn_groups.find(x => x.furn_id === f.furn_id)?.table_group_id}
+        student={name}
+        dragHandlers={{
+          onStart: (e, draggable) => onStartDrag(f),
+          onDrag: (e, draggable) => onDrag(f, draggable),
+          onStop: (e, draggable) => onStopDrag(f),
+        }}
+        rotateHandlers={{
+          onDrag: (e, draggable) => onRotate(f, e),
+          onStop: (e, draggable) => onStopDrag(f),
+        }} />
+    );
+  });
 
-        <div className="p-5 bg-gray-200 flex gap-2 items-center text-sm">
+  return (
+    <div className="h-full flex flex-col">
 
-          <HandRaisedIcon
-            className={`block h-8 w-8 p-1 cursor-pointer rounded
-                        ${this.state.mode === "select" ? "bg-slate-400" : "hover:bg-slate-300"}`}
-            onClick={() => this.setState({ mode: "select" })} />
-          <PlusIcon
-            className={`block h-8 w-8 p-1 cursor-pointer rounded
-                        ${this.state.mode === "place" ? "bg-slate-400" : "hover:bg-slate-300"}`}
-            onClick={() => this.setState({ mode: "place" })} />
+      <div className="p-5 bg-gray-200 flex gap-2 items-center text-sm">
+        <HandRaisedIcon
+          className={`block h-8 w-8 p-1 cursor-pointer rounded
+                        ${mode === "select" ? "bg-slate-400" : "hover:bg-slate-300"}`}
+          onClick={() => setMode("select")} />
+        <PlusIcon
+          className={`block h-8 w-8 p-1 cursor-pointer rounded
+                        ${mode === "place" ? "bg-slate-400" : "hover:bg-slate-300"}`}
+          onClick={() => setMode("place")} />
 
-          <div className="flex-1" />
+        <div className="flex-1" />
 
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={() => { this.mapFurniture() }}>
-            Group furniture
-          </button>
-          <button
-            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-            onClick={() => { this.clearStudents() }}>
-            Clear students
-          </button>
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={() => { this.mapStudents() }}>
-            Assign students
-          </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => { mapFurniture() }}>
+          Group furniture
+        </button>
+        <button
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => { clearStudents() }}>
+          Clear students
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => { mapStudents() }}>
+          Assign students
+        </button>
 
-        </div>
+        {
+          fsHandle.active ?
+            <ArrowsPointingInIcon
+              className={`block h-8 w-8 p-1 cursor-pointer rounded`}
+              onClick={fsHandle.exit} />
+            :
+            <ArrowsPointingOutIcon
+              className={`block h-8 w-8 p-1 cursor-pointer rounded`}
+              onClick={fsHandle.enter} />
+        }
+      </div>
 
+      <FullScreen
+        handle={fsHandle}
+        className="relative border-solid border border-slate-200 flex-1 bg-white">
         <div
-          className="relative border-solid border border-slate-200 flex-1"
-          ref={e => { this.divElement = e }}
-          onMouseDown={e => this.onMouseDown(e)} onTouchStart={e => this.onMouseDown(e)}
-          onMouseMove={e => this.onMouseMove(e)} onTouchMove={e => this.onMouseMove(e)}
-          onMouseUp={e => this.onMouseUp(e)} onTouchEnd={e => this.onMouseUp(e)}
+          className="h-full"
+          ref={divElement}
+          onMouseDown={e => onMouseDown(e)} onTouchStart={e => onMouseDown(e)}
+          onMouseMove={e => onMouseMove(e)} onTouchMove={e => onMouseMove(e)}
+          onMouseUp={e => onMouseUp(e)} onTouchEnd={e => onMouseUp(e)}
           tabIndex={0}>
 
           {seats}
 
         </div>
+      </FullScreen>
 
-      </div>
-    );
-  }
+    </div>
+  );
+
 }
